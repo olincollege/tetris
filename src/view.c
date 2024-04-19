951b98740e264ba2c50208d4310ff7b8b1ce84ad
Shree Madan <smadan@olin.edu>
2024-04-19 15:14:05 -0400

    Adding grid lines to pieces

--- src/view.c
+++ src/view.c
@@ -17,28 +17,30 @@ void render(SDL_Renderer* renderer, tetromino* tetrominos, int* bag,
                              SQUARE_WIDTH, SQUARE_WIDTH};
 
       // adding grid lines
-      SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
-      SDL_RenderDrawRect(renderer, &shape_rect);
       if (board_state[j][i].filled == 1) {
         SDL_SetRenderDrawColor(
             renderer, board_state[j][i].color.r, board_state[j][i].color.g,
             board_state[j][i].color.b, board_state[j][i].color.a);
         SDL_RenderFillRect(renderer, &shape_rect);
       }
+      SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
+      SDL_RenderDrawRect(renderer, &shape_rect);
     }
   }
 
-  SDL_SetRenderDrawColor(renderer, shape.color.r, shape.color.g, shape.color.b,
-                         shape.color.a);
   for (int i = 0; i < shape.cols; i++) {
     for (int j = 0; j < shape.rows; j++) {
+      SDL_Rect shape_rect = {
+          (int)(position[0] * SQUARE_WIDTH + i * SQUARE_WIDTH),
+          (int)(position[1] * SQUARE_WIDTH + j * SQUARE_WIDTH), SQUARE_WIDTH,
+          SQUARE_WIDTH};
       if (shape.shape[j][i] == 1) {
-        SDL_Rect shape_rect = {
-            (int)(position[0] * SQUARE_WIDTH + i * SQUARE_WIDTH),
-            (int)(position[1] * SQUARE_WIDTH + j * SQUARE_WIDTH), SQUARE_WIDTH,
-            SQUARE_WIDTH};
+        SDL_SetRenderDrawColor(renderer, shape.color.r, shape.color.g,
+                               shape.color.b, shape.color.a);
         SDL_RenderFillRect(renderer, &shape_rect);
       }
+      SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
+      SDL_RenderDrawRect(renderer, &shape_rect);
     }
   }
 
